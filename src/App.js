df1b7373d08505f0ce3cf92e157ae10803128c6a
jimareed <jim.a.reed@gmail.com>
2020-11-08 16:23:00 -0500

    added a callback route
    added a callback route

--- src/App.js
+++ src/App.js
@@ -4,11 +4,34 @@ import LoginButton from './LoginButton';
 import LogoutButton from './LogoutButton';
 import Profile from './Profile';
 import Authorize from './Authorize';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/callback">
+            <Callback />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+
+  return  <div className="App">
       <header className="App-header">
         <p>
           Sample react app to login to auth0 using authorize flow with PKCE.
@@ -18,8 +41,12 @@ function App() {
         <Profile/>
         <Authorize/>
       </header>
-    </div>
-  );
+    </div>;
+
+}
+
+function Callback() {
+  return <h2>Callback</h2>;
 }
 
 export default App;
